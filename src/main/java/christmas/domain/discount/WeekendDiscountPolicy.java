ab89dcc2bd0a372098b6cd2f5b720f8754a2d97a
kgy1008 <gayeon1008@g.skku.edu>
2024-11-25 23:40:31 +0900

    style: 줄바꿈 삭제

--- src/main/java/christmas/domain/discount/WeekendDiscountPolicy.java
+++ src/main/java/christmas/domain/discount/WeekendDiscountPolicy.java
@@ -7,7 +7,6 @@ public class WeekendDiscountPolicy implements DiscountPolicy {
 
     private static final int DISCOUNT_AMOUNT = 2023;
 
-
     @Override
     public int calculateDiscountAmount(final Date date, final Order order) {
         if (date.isWeekend() && order.isMainDish()) {
