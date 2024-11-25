181e76cfe117d1dea2fdc0c8b45f6bbf5c9220f5
kgy1008 <gayeon1008@g.skku.edu>
2024-11-25 23:20:25 +0900

    feat: 크리스마스 디데이 할인 금액 계산

--- src/main/java/christmas/domain/discount/ChristmasDiscountPolicy.java
+++ src/main/java/christmas/domain/discount/ChristmasDiscountPolicy.java
@@ -1,4 +1,24 @@
 package christmas.domain.discount;
 
+import christmas.domain.Date;
+
 public class ChristmasDiscountPolicy implements DiscountPolicy {
+
+    private static final int START_DATE = 1;
+    private static final int END_DATE = 25;
+    private static final int INIT_DISCOUNT_PRICE = 1000;
+    private static final int DISCOUNT_PRICE_UNIT = 100;
+
+    @Override
+    public int calculateDiscountAmount(final Date date) {
+        if (isDiscountApplicable(date)) {
+            int period = date.getDate() - START_DATE;
+            return INIT_DISCOUNT_PRICE + (period * DISCOUNT_PRICE_UNIT);
+        }
+        return 0;
+    }
+
+    private boolean isDiscountApplicable(final Date date) {
+        return date.getDate() >= START_DATE && date.getDate() <= END_DATE;
+    }
 }
