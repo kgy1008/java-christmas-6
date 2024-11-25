f58ac55440aaf674a60be36cb6aaa20163948eb9
kgy1008 <gayeon1008@g.skku.edu>
2024-11-25 16:30:46 +0900

    feat: 에러 메세지 정의

---
+++ src/main/java/christmas/common/ErrorMessage.java
@@ -0,0 +1,20 @@
+package christmas.common;
+
+public enum ErrorMessage {
+
+    INVALID_INPUT_TYPE("숫자로 입력해주세요."),
+    INVALID_INPUT_RANGE("12월 1일부터 12월 31일 사이의 날짜를 입력해주세요."),
+    NOT_FOUND_DAY("날짜에 해당하는 요일을 찾을 수 없습니다.")
+    ;
+
+    private static final String HEADER = "[ERROR ]";
+    private final String message;
+
+    ErrorMessage(final String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return HEADER + message;
+    }
+}
