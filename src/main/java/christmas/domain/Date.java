d4f6ddb833a7fbb3d70f01b4b49dd6c56505e124
kgy1008 <gayeon1008@g.skku.edu>
2024-11-25 23:10:41 +0900

    feat: 달력에 별 모양이 적용된 날짜인지 구분하는 필드 추가

--- src/main/java/christmas/domain/Date.java
+++ src/main/java/christmas/domain/Date.java
@@ -4,23 +4,30 @@ import static christmas.common.ErrorMessage.INVALID_INPUT_RANGE;
 
 public class Date {
 
-    private static final int MIN_DAY = 1;
-    private static final int MAX_DAY = 31;
+    private static final int MIN_DATE = 1;
+    private static final int MAX_DATE = 31;
+    private static final int CHRISTMAS_DATE = 25;
 
-    private final int day;
+    private final int date;
+    private final boolean isSpecialDate;
 
-    public Date(final int day) {
-        validateRange(day);
-        this.day = day;
+    public Date(final int date) {
+        validateRange(date);
+        this.date = date;
+        this.isSpecialDate = setIsStared();
     }
 
     private void validateRange(final int day) {
-        if (day < MIN_DAY || day > MAX_DAY) {
+        if (day < MIN_DATE || day > MAX_DATE) {
             throw new IllegalArgumentException(INVALID_INPUT_RANGE.getMessage());
         }
     }
 
+    private boolean setIsStared() {
+        return findDay() == Day.SUNDAY || date == CHRISTMAS_DATE;
+    }
+
     private Day findDay() {
-        return Day.find(day);
+        return Day.find(date);
     }
 }
