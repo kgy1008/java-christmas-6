3f5b8b2a67e50c7994d9e88f1ecaf7215e86a062
kgy1008 <gayeon1008@g.skku.edu>
2024-11-25 16:31:19 +0900

    feat: 12월 날짜 입력받기

--- src/main/java/christmas/view/InputView.java
+++ src/main/java/christmas/view/InputView.java
@@ -1,4 +1,29 @@
 package christmas.view;
 
+import static christmas.common.ErrorMessage.INVALID_INPUT_TYPE;
+
+import camp.nextstep.edu.missionutils.Console;
+import christmas.domain.Date;
+
 public class InputView {
+
+    public static Date inputVisitDay() {
+        System.out.println("12월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)");
+        String answer = Console.readLine();
+        int day = convertToInt(answer);
+        return new Date(day);
+    }
+
+    public static String inputMenu() {
+        System.out.println("주문하실 메뉴를 메뉴와 개수를 알려 주세요. (e.g. 해산물파스타-2,레드와인-1,초코케이크-1)");
+        return Console.readLine();
+    }
+
+    private static int convertToInt(final String input) {
+        try {
+            return Integer.parseInt(input);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException(INVALID_INPUT_TYPE.getMessage());
+        }
+    }
 }
