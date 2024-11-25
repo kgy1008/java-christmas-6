108e369535cad09165e446713cd6f9f8349e2ea1
kgy1008 <gayeon1008@g.skku.edu>
2024-11-25 23:37:13 +0900

    feat: 평일 할인액 계산

--- src/main/java/christmas/domain/discount/WeekdayDiscountPolicy.java
+++ src/main/java/christmas/domain/discount/WeekdayDiscountPolicy.java
@@ -1,4 +1,17 @@
 package christmas.domain.discount;
 
+import christmas.domain.Date;
+import christmas.domain.order.Order;
+
 public class WeekdayDiscountPolicy implements DiscountPolicy {
+
+    private static final int DISCOUNT_AMOUNT = 2023;
+
+    @Override
+    public int calculateDiscountAmount(final Date date, final Order order) {
+        if (date.isWeekday() && order.isDessert()) {
+            return DISCOUNT_AMOUNT * order.getCount();
+        }
+        return 0;
+    }
 }
