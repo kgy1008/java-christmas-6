51e33e3e5f6109caa4bbd533359ef7cbc971a94d
kgy1008 <gayeon1008@g.skku.edu>
2024-11-25 22:53:04 +0900

    feat: 사용자로부터 날짜 입력받기

--- src/main/java/christmas/view/InputView.java
+++ src/main/java/christmas/view/InputView.java
@@ -7,7 +7,7 @@ import christmas.domain.Date;
 
 public class InputView {
 
-    public static Date inputVisitDay() {
+    public static Date inputVisitDate() {
         System.out.println("12월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)");
         String answer = Console.readLine();
         int day = convertToInt(answer);
